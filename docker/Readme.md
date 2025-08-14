5793206242235b8b8a715fcddc377425ff92748a
Feroz Ahmmed <feroz.shah.940@gmail.com>
2025-08-14 11:19:29 +0600

    working example with promtail

--- docker/Readme.md
+++ docker/Readme.md
@@ -2,3 +2,4 @@
 ```dockerfile
 docker start docker-grafana-1 docker-alloy-1 docker-backend-1 docker-gateway-1 docker-read-1 docker-write-1 docker-minio-1             
 ```
+
--- docker/init-db/init-multiple-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE restaurant;
-    CREATE DATABASE payments;
-    CREATE DATABASE sales;
-EOSQL
